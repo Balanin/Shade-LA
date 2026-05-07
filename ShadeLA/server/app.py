e0cc6e0f5f412acfae521df0d2db55b6ec36e64b
Balan <vova.balan1988@gmail.com>
2026-05-07 13:55:17 -0700

    Allow CORS for GitHub Pages

--- ShadeLA/server/app.py
+++ ShadeLA/server/app.py
@@ -25,6 +25,9 @@ app = FastAPI(title="Terrain Solar Analysis API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://balanin.github.io",
+        "https://balanin.github.io/Shade-LA",
+        "https://balanin.github.io/Shade-LA/",
         "http://127.0.0.1:5173",
         "http://localhost:5173",
         "http://127.0.0.1:5174",
